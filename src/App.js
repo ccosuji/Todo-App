c8b07bf2d4f072635543dfbd046ccfadc88bb21c
cjfactor <cjfactor.o@gmail.com>
2020-11-03 13:23:24 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './Components/Todos';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take Out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Dinner with wife',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'Call Mum',
+                completed: false
+            },
+        ]
+    }
+    render() {
+
+        return (
+            <div className="App">
+                <Todos todos={this.state.todos} />
+
+
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/Components/Todos.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Todoitems from './Todoitems';
+class Todos extends Component {
+    render() {
+        return this.props.todos.map((todo) => (
+            <Todoitems />
+        ))
+    }
+
+}
+
+export default Todos;
